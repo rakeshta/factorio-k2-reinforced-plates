3a5c233f53c5b5a8fb3d20bc6614e4fe90e696dc
Rakesh Ayyaswami <rakeshta@gmail.com>
2023-01-03 01:33:19 +1100

    skeleton project

---
+++ data.lua
@@ -0,0 +1,9 @@
+--
+--  data.lua
+--  factorio-k2-reinforced-plates
+--
+--  Created by Rakesh Ayyaswami on 03 Jan 2023.
+--
+
+--- Path to the mod's root directory
+k2_reinforced_plates = "__k2-reinforced-plates__/"
